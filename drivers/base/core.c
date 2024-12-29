14ebfd223949f1fa2b009cffc4139f4cd48ebde1
Mizumo-prjkt <bluepearl394@gmail.com>
2024-12-30 05:30:49 +0800

    Revert "driver core: Fix adding device links to probing suppliers"
    This reverts commit 6a425995b3a2c3f518fb4bfd6668650f222e3d47.

--- drivers/base/core.c
+++ drivers/base/core.c
@@ -399,26 +399,17 @@ struct device_link *device_link_add(struct device *consumer,
 		link->status = DL_STATE_NONE;
 	} else {
 		switch (supplier->links.status) {
-		case DL_DEV_PROBING:
-			switch (consumer->links.status) {
-			case DL_DEV_PROBING:
-				/*
-				 * A consumer driver can create a link to a
-				 * supplier that has not completed its probing
-				 * yet as long as it knows that the supplier is
-				 * already functional (for example, it has just
-				 * acquired some resources from the supplier).
-				 */
-				link->status = DL_STATE_CONSUMER_PROBE;
-				break;
-			default:
-				link->status = DL_STATE_DORMANT;
-				break;
-			}
-			break;
 		case DL_DEV_DRIVER_BOUND:
 			switch (consumer->links.status) {
 			case DL_DEV_PROBING:
+				/*
+				 * Some callers expect the link creation during
+				 * consumer driver probe to resume the supplier
+				 * even without DL_FLAG_RPM_ACTIVE.
+				 */
+				if (flags & DL_FLAG_PM_RUNTIME)
+					pm_runtime_resume(supplier);
+
 				link->status = DL_STATE_CONSUMER_PROBE;
 				break;
 			case DL_DEV_DRIVER_BOUND:
@@ -438,14 +429,9 @@ struct device_link *device_link_add(struct device *consumer,
 		}
 	}
 
-	/*
-	 * Some callers expect the link creation during consumer driver probe to
-	 * resume the supplier even without DL_FLAG_RPM_ACTIVE.
-	 */
-	if (link->status == DL_STATE_CONSUMER_PROBE &&
-	    flags & DL_FLAG_PM_RUNTIME)
-		pm_runtime_resume(supplier);
-
+	if (flags & DL_FLAG_SYNC_STATE_ONLY)
+		goto out;
+reorder:
 	/*
 	 * Move the consumer and all of the devices depending on it to the end
 	 * of dpm_list and the devices_kset list.
@@ -860,17 +846,7 @@ void device_links_driver_bound(struct device *dev)
 	device_links_write_lock();
 
 	list_for_each_entry(link, &dev->links.consumers, s_node) {
-		if (!(link->flags & DL_FLAG_MANAGED))
-			continue;
-
-		/*
-		 * Links created during consumer probe may be in the "consumer
-		 * probe" state to start with if the supplier is still probing
-		 * when they are created and they may become "active" if the
-		 * consumer probe returns first.  Skip them here.
-		 */
-		if (link->status == DL_STATE_CONSUMER_PROBE ||
-		    link->status == DL_STATE_ACTIVE)
+		if (link->flags & DL_FLAG_STATELESS)
 			continue;
 
 		WARN_ON(link->status != DL_STATE_DORMANT);
@@ -934,7 +910,7 @@ static void __device_links_no_driver(struct device *dev)
 			continue;
 
 		if (link->flags & DL_FLAG_AUTOREMOVE_CONSUMER)
-			kref_put(&link->kref, __device_link_del);
+			__device_link_del(&link->kref);
 		else if (link->status != DL_STATE_SUPPLIER_UNBIND)
 			WRITE_ONCE(link->status, DL_STATE_AVAILABLE);
 	}
@@ -942,40 +918,10 @@ static void __device_links_no_driver(struct device *dev)
 	dev->links.status = DL_DEV_NO_DRIVER;
 }
 
-/**
- * device_links_no_driver - Update links after failing driver probe.
- * @dev: Device whose driver has just failed to probe.
- *
- * Clean up leftover links to consumers for @dev and invoke
- * %__device_links_no_driver() to update links to suppliers for it as
- * appropriate.
- *
- * Links without the DL_FLAG_MANAGED flag set are ignored.
- */
 void device_links_no_driver(struct device *dev)
 {
-	struct device_link *link;
-
 	device_links_write_lock();
-
-	list_for_each_entry(link, &dev->links.consumers, s_node) {
-		if (!(link->flags & DL_FLAG_MANAGED))
-			continue;
-
-		/*
-		 * The probe has failed, so if the status of the link is
-		 * "consumer probe" or "active", it must have been added by
-		 * a probing consumer while this device was still probing.
-		 * Change its state to "dormant", as it represents a valid
-		 * relationship, but it is not functionally meaningful.
-		 */
-		if (link->status == DL_STATE_CONSUMER_PROBE ||
-		    link->status == DL_STATE_ACTIVE)
-			WRITE_ONCE(link->status, DL_STATE_DORMANT);
-	}
-
 	__device_links_no_driver(dev);
-
 	device_links_write_unlock();
 }
 
