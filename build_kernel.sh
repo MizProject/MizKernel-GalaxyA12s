9cb65a0fbcdc9de4f469b51f4bc79d1be56b4df0
Mizumo-prjkt <bluepearl394@gmail.com>
2025-01-03 00:09:55 +0800

    change
    and also f##k intellisense making my laptop from core to atom

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,5 +5,6 @@ export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=t
 export ARCH=arm64
 
-make ARCH=arm64 exynos850-a12snsxx_defconfig
+make clean && make mrproper
+make ARCH=arm64 mizkernel-a12snsxx_defconfig
 make ARCH=arm64 -j64
