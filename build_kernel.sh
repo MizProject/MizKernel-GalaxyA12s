5983b4b22fe4b0d9ea4c09e7b42597a9575288f4
Mizu <70519093+Mizumo-prjkt@users.noreply.github.com>
2025-01-07 05:06:47 +0000

    feat: add new submodules for aarch64 toolchain and clang

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
 
+# export PLATFORM_VERSION=13
+# export ANDROID_MAJOR_VERSION=t
+# export ARCH=arm64
+
+# make clean && make mrproper
+# make ARCH=arm64 mizkernel-a12snsxx_defconfig
+# make ARCH=arm64 -j64
+
+
 export PLATFORM_VERSION=13
 export ANDROID_MAJOR_VERSION=t
 export ARCH=arm64
+export KBUILD_BUILD_USER="@Mizumo_prjkt"
 
-make clean && make mrproper
-make ARCH=arm64 mizkernel-a12snsxx_defconfig
-make ARCH=arm64 -j64
+# Summon KSU
+
+git-init_ksu() {
+    git submodule init && git submodule update
+}
+
+START_BUILD_TIME_RAW=$(TZ="Asia/Manila" date +%T)
+START_BUILD_DATE_RAW=$(TZ="Asia/Manila" date +%F)
+START_BUILD_TIME_AND_DATE="$START_BUILD_TIME_RAW - $START_BUILD_DATE_RAW"
+echo "Starting Compile of A12s Kernel"
+echo "Start Build: $START_BUILD_TIME_AND_DATE"
+
+
